066db7a6dc5bc87645f120f149ccb039266dda4a
lukmanul160 <lukmanul160@gmail.com>
2020-09-30 19:26:40 +0700

    commit 1

---
+++ framework-4.0.4/app/Entities/Users.php
@@ -0,0 +1,14 @@
+<?php namespace App\Entities;
+use CodeIgniter\Entity;
+
+class Users extends Entity
+{
+    public function setPassword(string $pass)
+    {
+        $salt = uniqid('', true);
+        $this->attributes['salt'] = $salt;
+        $this->attributes['password'] = md5($salt.$pass);
+
+        return $this;
+    }
+}
